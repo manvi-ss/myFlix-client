8226e0741917d77c1aa938f4acc055e0d97d9e86
Manvi <131688388+manvi-ss@users.noreply.github.com>
2023-07-24 23:18:22 +0200

    movie Card is given Link based on react routes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,30 +1,35 @@
 import PropTypes from "prop-types";
-import {Card} from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({movie, onMovieClick}) => {
-    return (
-    <Card onClick={() => onMovieClick(movie)} variant="link"  className = " movieCard" style={{ width: "18rem" , height:"100%" }}>
-      <Card.Img variant="top" src={movie.ImagePath} />
-      <Card.Body>
-        <Card.Title> {movie.Title} </Card.Title>
-      </Card.Body>
-    </Card>
-    );
+export const MovieCard = ({ movie }) => {
+  return (
+    <Link
+      className="text-decoration-none"
+      to={`/movies/${encodeURIComponent(movie.id)}`}
+    >
+      <Card style={{ width: "18rem", height: "100%" }}>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body className="text-center">
+          <Card.Title> {movie.Title} </Card.Title>
+        </Card.Body>
+      </Card>
+    </Link>
+  );
 };
 MovieCard.propTypes = {
-    movie: PropTypes.shape({
-        _id: PropTypes.string.isRequired,
-        Title: PropTypes.string.isRequired,
-        Genre: PropTypes.shape({
-            Name: PropTypes.string.isRequired,
-            Description: PropTypes.string.isRequired
-        }).isRequired,
-        Director: PropTypes.shape({
-            Name: PropTypes.string.isRequired,
-            Bio: PropTypes.string.isRequired,
-            Birth: PropTypes.string.isRequired
-        }).isRequired,
-        ImagePath:PropTypes.string.isRequired,
-        Featured: PropTypes.bool.isRequired
-    }).isRequired
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired,
+    }).isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Featured: PropTypes.bool.isRequired,
+  }).isRequired,
 };
