238df880daf83b9054b56ea0523523543638f82a
Manvi <131688388+manvi-ss@users.noreply.github.com>
2023-07-27 00:56:59 +0200

    Revert "Navigationbar"

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -5,7 +5,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand to="/">myFlix</Navbar.Brand>
+        <Navbar.Brand to="/">Movies</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
